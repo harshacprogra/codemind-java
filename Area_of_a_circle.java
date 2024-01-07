b0ee1b3cc35b2f15e2c769a9e892f8de66c8c8fc
Harsha Vardhan <99875579+harshacprogra@users.noreply.github.com>
2024-01-07 23:49:13 +0530

    import java.util.*;
    public class r4 {
      public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        int n = sc.nextInt();
        System.out.printf("%.2f", (n * n * 3.14));
      }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class r4 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.printf("%.2f", (n * n * 3.14));
+  }
+}
