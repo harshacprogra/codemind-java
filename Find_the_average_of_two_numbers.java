8173db43ce51a3f19376fbde3c3ac422eb8ce3ff
Harsha Vardhan <99875579+harshacprogra@users.noreply.github.com>
2024-01-07 23:43:44 +0530

    import java.util.*;
    public class r4 {
      public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        float n = sc.nextInt();
        float m = sc.nextInt();
        float avg = (n + m) / 2;
        System.out.printf("%.4f", avg);
      }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class r4 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    float n = sc.nextInt();
+    float m = sc.nextInt();
+    float avg = (n + m) / 2;
+    System.out.printf("%.4f", avg);
+  }
+}
